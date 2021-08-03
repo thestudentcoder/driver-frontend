ac7755fb41c20b611177cdd2c2d283d1e827440d
Wayne Chan <wchan.it@outlook.com>
2021-08-03 02:20:36 -0500

    Add landing page and Drivers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,13 @@ const Header = () => {
     <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src={'https://placeholder.pics/svg/150x50/888888/EEE/Logo'}
+              alt="logo"
+              height="36"
+            />
+          </Navbar.Brand>
         </LinkContainer>
         <Nav className="mr-auto">
           <LinkContainer to="/search">
